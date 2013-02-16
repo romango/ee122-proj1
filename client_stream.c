dc60b2a909c2ee98637f2c3510a06748ef79807c
Rohin Jethani <rohin.jethani@gmail.com>
2013-02-16 12:48:57 -0800

    adding unfinished client stream file

---
+++ client_stream.c
@@ -0,0 +1,16 @@
+/* Client Stream */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT "4444"
+#define MAXDATASIZE 100
+
+
